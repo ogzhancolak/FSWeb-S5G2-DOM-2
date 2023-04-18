0053c30fe36c79ad47356cbeb831b0a7e04b194b
Lucky <lucky@Lucky-Air.bbrouter>
2023-04-19 02:23:56 +0300

    DOM2-Tamamlandı

--- src/index.js
+++ src/index.js
@@ -7,3 +7,50 @@ import './less/index.less'
 
 
 // Kodlar buraya gelecek!
+
+document.querySelector("h1").addEventListener("click",function(e){
+     alert("Bana tıkladın!")
+});
+
+const yönlendir = (url) => {
+    document.location.href = url;
+}
+
+document.getElementsByClassName('btn')[0].addEventListener("click", function(e){
+    yönlendir('https://www.rixos.com/tr');
+})
+
+document.getElementsByClassName('btn')[1].addEventListener("click", function(e){
+    yönlendir('https://www.jollytur.com/karadeniz-turlari');
+})
+
+document.getElementsByClassName('btn')[2].addEventListener("click", function(e){
+    yönlendir('https://www.datcadetay.com/datca-gezi-tekneleri.html');
+})
+
+const mouse = document.querySelectorAll('.nav-link')
+
+mouse.forEach((click => {
+    click.addEventListener('mouseover', (event) => {
+        event.target.style.color = 'darkorange';
+        click.style.fontWeight = 'bold';
+    })
+    click.addEventListener('mouseout', (event) => {
+        event.target.style.color = 'black';
+        click.style.fontWeight = 'normal';
+    })
+}))
+
+const button = document.querySelectorAll('.btn');
+
+button.forEach(abutton => {
+    abutton.addEventListener('mouseenter', (event) => {
+        event.target.style.background = "orange";
+    })
+    
+    abutton.addEventListener('mouseout', (event) => {
+        event.target.style.background = "";
+    })
+})
+
+
